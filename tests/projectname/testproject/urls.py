fd968058fac13fca090e14a2423e5f0a5347f419
Soldatov Serhii <soldatov.own@gmail.com>
2026-04-26 09:36:04 +0200

    correct test project naming

--- tests/projectname/testproject/urls.py
+++ tests/projectname/testproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projectname project.
+URL configuration for testproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
